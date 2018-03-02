fae844dc65fc3f147c995ab1572e881cb9791903
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-03-02 14:41:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,5 +19,9 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
-
+  get "/multiply" do
+    @num1 = params[:num1]
+    @num2 = params[:num2]
+    @num1 * @num2
+  end
 end
